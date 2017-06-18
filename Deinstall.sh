6c2057132c2b29ec4fa9449214cd7110f1c38cda
Mathias Renner <mail@mathias-renner.com>
2017-06-19 00:39:20 +0200

    Smaller fixes in Deinstall script

--- Deinstall.sh
+++ Deinstall.sh
@@ -6,28 +6,23 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-read -r -p "Are you sure? [y/N] " response
+read -r -p "Are you sure to completely remove Invoiceninja? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
-
 
 echo -e "\e[100mStep 1/4 \e[44m Stopping invoiceninja...\e[0m"
 instop
 
-
-echo -e "\e[100mStep 1/4 \e[44m Removing related Docker images...\e[0m"
+echo -e "\e[100mStep 2/4 \e[44m Removing related Docker images...\e[0m"
 docker rmi invoiceninja/invoiceninja
 docker rmi mariadb
 echo Done.
 
-
-echo -e "\e[100mStep 1/4 \e[44m Removing invoiceninja folder...\e[0m"
+echo -e "\e[100mStep 3/4 \e[44m Removing invoiceninja folder...\e[0m"
 rm -rf $(cat ~/.bashrc | grep instart |  grep -o -P '(?<=-f).*(?=invoiceninja-docker/docker-compose.yml)')
 
-
-echo -e "\e[100mStep 1/4 \e[44m Removing aliases...\e[0m"
+echo -e "\e[100mStep 4/4 \e[44m Removing aliases...\e[0m"
 sed -i "/\b\(invoiceninja|instart|instop|inclean|inbackup)\b/d" ~/.bashrc
-
 source ~/.bashrc
 
 echo Done.
