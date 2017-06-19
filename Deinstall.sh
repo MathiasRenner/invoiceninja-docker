8f4967eb122ec0ba49d3fb191f57b3bfc09069c1
Mathias Renner <mail@mathias-renner.com>
2017-06-19 02:08:08 +0200

    Many fixes and improvents to deinstall script

--- Deinstall.sh
+++ Deinstall.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 set -e
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root."
-  exit
-fi
-
 read -r -p "Are you sure to completely remove Invoiceninja? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
 
 echo -e "\e[100mStep 1/4 \e[44m Stopping invoiceninja...\e[0m"
-instop
+docker-compose -f $(cat ~/.bashrc | grep instop |  grep -o -P '(?<=-f).*(?=stop)') down
+OK
 
 echo -e "\e[100mStep 2/4 \e[44m Removing related Docker images...\e[0m"
-docker rmi invoiceninja/invoiceninja
-docker rmi mariadb
-echo Done.
+docker rmi invoiceninja/invoiceninja || true
+docker rmi mariadb || true
+echo OK
 
-echo -e "\e[100mStep 3/4 \e[44m Removing invoiceninja folder...\e[0m"
-rm -rf $(cat ~/.bashrc | grep instart |  grep -o -P '(?<=-f).*(?=invoiceninja-docker/docker-compose.yml)')
-
-echo -e "\e[100mStep 4/4 \e[44m Removing aliases...\e[0m"
-sed -i "/\b\(invoiceninja|instart|instop|inclean|inbackup)\b/d" ~/.bashrc
+echo -e "\e[100mStep 3/4 \e[44m Removing aliases...\e[0m"
+sed -i "/\b\(invoiceninja\|instart\|instop\|inclean\|inbackup\)\b/d" ~/.bashrc
 source ~/.bashrc
+echo OK
 
-echo Done.
+echo -e "\e[100mStep 4/4 \e[44m Removing invoiceninja folder...\e[0m"
+cd $(cat $HOMEDIR/.bashrc | grep instart |  grep -o -P '(?<=-f).*(?=docker-compose.yml)')
+echo Sudo is required to move forward...
+sudo rm -rf invoiceninja-docker
+
+echo All done.
 
 else
   echo Cancelled by user. Exiting...
