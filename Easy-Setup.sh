376b002a8df9e919e22da433da1de04a283118da
Mathias Renner <MathiasRenner@users.noreply.github.com>
2017-07-29 21:46:08 +0200

    Add choice for user to set up email config or not

--- Easy-Setup.sh
+++ Easy-Setup.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 set -e
 
-echo -e "\e[100mStep 1/4 \e[44m Collecting user settings...\e[0m"
+echo -e "Would you like to configure email now? Then type 'y'. If you want to configure it later, type 'n':"
+read usersettings
 
-echo "Type your email adress (e.g. mail@example.com) [ENTER]:"
-read mailaddress
+if [[ $usersettings == y ]]; then
 
-echo "Type your email host address (e.g. mail.example.com) and then press [ENTER]:"
-read mailhost
+    echo -e "\e[100mStep 1/4 \e[44m Collecting user settings...\e[0m"
 
-echo "Type your email username (often identical with your email address) [ENTER]:"
-read mailusername
+    echo "Type your email adress (e.g. mail@example.com) [ENTER]:"
+    read mailaddress
 
-echo "Type your email password [ENTER]:"
-read mailpassword
+    echo "Type your email host address (e.g. mail.example.com) and then press [ENTER]:"
+    read mailhost
 
-echo "Type the name the email receiver should see as email sender (often full personal or company name) [ENTER]:"
-read mailfromname
+    echo "Type your email username (often identical with your email address) [ENTER]:"
+    read mailusername
 
+    echo "Type your email password [ENTER]:"
+    read mailpassword
+
+    echo "Type the name the email receiver should see as email sender (often full personal or company name) [ENTER]:"
+    read mailfromname
+fi
 
 echo -e "\e[100mStep 2/5 \e[44m Cloning repository...\e[0m"
 git clone https://github.com/MathiasRenner/invoiceninja-docker.git
@@ -25,16 +30,22 @@ cd invoiceninja-docker
 echo OK
 
 
-echo -e "\e[100mStep 3/5 \e[44m Writing user config...\e[0m"
+if [[ $usersettings == y ]]; then
 
-sed -i -- "s/mail@example.com/$mailaddress/" docker-compose.yml
-sed -i -- "s/mail.service.host/$mailhost/" docker-compose.yml
-sed -i -- "s/username/$mailusername/" docker-compose.yml
-sed -i -- "s/password/$mailpassword/" docker-compose.yml
-sed -i -- "s/fromname/$mailfromname/" docker-compose.yml
+    echo -e "\e[100mStep 3/5 \e[44m Writing user config...\e[0m"
 
-echo OK
+    sed -i -- "s/mail@example.com/$mailaddress/" docker-compose.yml
+    sed -i -- "s/mail.service.host/$mailhost/" docker-compose.yml
+    sed -i -- "s/username/$mailusername/" docker-compose.yml
+    sed -i -- "s/password/$mailpassword/" docker-compose.yml
+    sed -i -- "s/fromname/$mailfromname/" docker-compose.yml
 
+    echo OK
+fi
+
+if [[ $usersettings == n ]]; then
+  echo -e "\e[100mStep 2/5 \e[44m No user config available. Skipping to write user config ...\e[0m"
+fi
 
 echo -e "\e[100mStep 4/5 \e[44m Pulling neccessary Docker images...\e[0m"
 docker-compose pull
